698ab0db32684bc502b727792219be8eaa05e73e
Eden Sainty <eden.sainty@gmail.com>
2020-06-22 22:08:00 +1000

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Hardcodet.Wpf.TaskbarNotification;
+
+namespace EmployeeCentre {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+
+        private TaskbarIcon notifyIcon;
+
+        protected override void OnStartup(StartupEventArgs e) {
+            base.OnStartup(e);
+
+            var icon = FindResource("notifyIcon");
+
+            notifyIcon = (TaskbarIcon)FindResource("notifyIcon");
+        }
+    }
+}
---
+++ EmployeeCentreViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace EmployeeCentre {
+    class EmployeeCentreViewModel {
+        public ICommand showWindowCommand { 
+            get {
+                return new DelegateCommand {
+                    CanExecuteFunc = () => {
+                        return (Application.Current.MainWindow == null || Application.Current.MainWindow.IsVisible == false);
+                    },
+
+                    CommandAction = () => {
+                        Application.Current.MainWindow = new MainWindow();
+                        Application.Current.MainWindow.Show();
+                    },
+                };
+            } 
+        }
+
+        public ICommand hideWindowCommand {
+            get {
+                return new DelegateCommand {
+                    CanExecuteFunc = () => {
+                        return (Application.Current.MainWindow != null && Application.Current.MainWindow.IsVisible);
+                    },
+
+                    CommandAction = () => {
+                        Application.Current.MainWindow = new MainWindow();
+                        Application.Current.MainWindow.Show();
+                    },
+                };
+            }
+        }
+    }
+
+    class DelegateCommand : ICommand {
+        public Action CommandAction { get; set; }
+        public Func<bool> CanExecuteFunc { get; set; }
+
+        public event EventHandler CanExecuteChanged {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter) {
+            return CanExecuteFunc();
+        }
+
+        public void Execute(object parameter) {
+            CommandAction();
+        }
+    }
+}
