4b9b8138f982d614f53c0a89aa43fa1f505379ac
Eden Sainty <eden.sainty@gmail.com>
2020-06-23 21:24:44 +1000

    Updated to use dedicated managed thread for background management process.

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,35 +20,38 @@ namespace EmployeeCentre {
 
         private TaskbarIcon notifyIcon;
 
-        Task backgroundWorker;
-        DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
-
         protected override void OnStartup(StartupEventArgs e) {
             base.OnStartup(e);
 
             var icon = FindResource("notifyIcon");
-
             notifyIcon = (TaskbarIcon)FindResource("notifyIcon");
 
-            backgroundWorker = workLoop();
+            CancellationTokenSource cts = new CancellationTokenSource();
+            ThreadPool.QueueUserWorkItem(workerThreadMain, cts.Token);
         }
 
-        private async Task workLoop() {
+        private void workerThreadMain(object state) {
+            CancellationToken token = (CancellationToken)state;
+            Timer timer = new Timer(timerCallback, null, 0, 5000);
             while (true) {
-                timer.Interval = new TimeSpan(0, 0, 5);
-                timer.Tick += new EventHandler(async (object sender, System.EventArgs e) => {
-                    this.Dispatcher.Invoke(() => {
-                        if (Current.MainWindow == null) {
-                            Current.MainWindow = new MainWindow();
-                            Current.MainWindow.Show();
-                        } else {
-                            Current.MainWindow.Show();
-                        }
-                    });
-                });
-                timer.Start();
-            }   
+                Thread.Sleep(1000);
+                if (token.IsCancellationRequested) {
+                    break;
+                }
+            }
         }
+
+        private void timerCallback(object state) {
+            Dispatcher.Invoke(() => {
+                if (Current.MainWindow == null) {
+                    Current.MainWindow = new MainWindow();
+                    Current.MainWindow.Show();
+                } else {
+                    Current.MainWindow.Show();
+                }
+            });
+        }
+
 
         public async void ScheduleAction(Action action, DateTime executionTime) {
             Current.MainWindow.Show();
