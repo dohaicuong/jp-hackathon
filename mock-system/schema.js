5437fec4480d7b61d02664969d533dc5fa5e513c
dohaicuong <beatyshot@gmail.com>
2020-06-25 22:37:36 +1000

    schema update

--- mock-system/schema.js
+++ mock-system/schema.js
@@ -1,25 +1,93 @@
 const { gql } = require('apollo-server')
 
 module.exports = gql`
-  ## TODO type structure decision
   scalar Frequency
+  scalar Answer
+  scalar DateTime
+
+  interface Node { id: ID! }
 
   # QUERY
+
   type Query {
-    org(id: ID!): OrgUnit
-    tasks: [NotificationTask!]
+    me: User!
+    node(id: ID!): Node
   }
 
-  interface Node {
+  # MUTATION
+
+  type Mutation {
+    signup(input: UserSignupInput!): AuthPayload
+    login(input: UserLoginInput!): AuthPayload
+
+    organisationCreate(input: OrgCreateInput!): OrgCreatePayload
+
+    reportSend(input: ReportSendInput!): ReportSendPayload
+  }
+
+  input UserSignupInput {
+    email: String!
+    password: String!
+    name: String!
+    role: String!
+  }
+
+  input UserLoginInput {
+    email: String!
+    password: String!
+  }
+
+  type AuthPayload {
+    token: String!
+    user: User!
+  }
+
+  input OrgCreateInput {
+    orgName: String!
+    divisionNames: [String!]
+    brancheNames: [String!]
+  }
+  type OrgCreatePayload {
+    organisation: Organisation
+  }
+
+
+  input ReportSendInput {
+    questionId: ID!
+    answer: Answer
+  }
+  type ReportSendPayload {
+    report: Report
+  }
+
+  # MODELS
+  type Account implements Node {
     id: ID!
+    email: String!
+    password: String!
+    user: User!
   }
 
-  # ORG
-  type OrgUnit implements Node {
+  type Organisation implements Node {
     id: ID!
     name: String!
 
-    orgUnits: [OrgUnit!]
+    divisions: [Division!]
+  }
+
+  type Division implements Node {
+    id: ID!
+    name: String!
+
+    organisation: Organisation!
+    branches: [Branch!]
+  }
+
+  type Branch implements Node {
+    id: ID!
+    name: String!
+
+    division: Division!
     employees: [User!]
   }
 
@@ -27,9 +95,12 @@ module.exports = gql`
     id: ID!
     name: String!
     role: String!
+
+    branch: Branch!
+    tasks: [NotificationTask!]
+    account: Account
   }
 
-  # TASK
   type NotificationTask implements Node {
     id: ID!
     questions: [Question!]
@@ -38,27 +109,28 @@ module.exports = gql`
     frequencies: [Frequency!]
   }
 
-  # QUESTION
-  interface Question {
-    question: String!
-  }
-
-  type RateQuestion implements Node & Question  {
+  type Question implements Node {
     id: ID!
 
     question: String!
-    options: [RateQuestionOption!]
-    answer: ID
+    options: [QuestionOption!]
+
+    isMultiple: Boolean!
+    isOptionalReponse: Boolean!
   }
-  type RateQuestionOption implements Node {
+  type QuestionOption implements Node {
     id: ID!
-    label: String!
+    label: String
+    logo: String
   }
 
-  type ResponseQuestion implements Node & Question {
+  type Report implements Node {
     id: ID!
+    
+    question: Question!
+    user: User!
 
-    question: String!
-    answer: String
+    answer: Answer!
+    createdAt: DateTime!
   }
 `
